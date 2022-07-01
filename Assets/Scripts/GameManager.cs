6b1df5a7cd23d4802d8eead874e46beca324e941
windless1015 <shangtao1989@qq.com>
2022-07-01 11:41:32 -0700

    modify some bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverCanvas;
     private void Start()
     {
+        gameOverCanvas.SetActive(false);
         Time.timeScale = 1;
     }
 

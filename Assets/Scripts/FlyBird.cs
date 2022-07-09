8981687d1e9b516835127f85e7b1bd66af360803
windless1015 <shangtao1989@qq.com>
2022-07-09 04:35:13 -0700

    add rotation when bird flys

--- Assets/Scripts/FlyBird.cs
+++ Assets/Scripts/FlyBird.cs
@@ -6,20 +6,50 @@ public class FlyBird : MonoBehaviour
 {
     public GameManager gameManager;
     public float verticalVelocity = 2.4f;
+    //public Vector2 jumpForce = new Vector2(0, 300);
     private Rigidbody2D birdRigBody;
     // Start is called before the first frame update
     void Start()
     {
         birdRigBody = GetComponent<Rigidbody2D>();
+
+        //birdRigBody.velocity = Vector2.zero;
+        //birdRigBody.AddForce(jumpForce);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             birdRigBody.velocity = Vector2.up * verticalVelocity;
-        }        
+
+            //birdRigBody.velocity = Vector2.zero;
+            //birdRigBody.AddForce(jumpForce);
+
+        }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.y < -1 * Screen.height)
+        {
+            gameManager.GameOver();
+        }
+
+        if (birdRigBody.velocity.y > 0)
+        {
+            // first is min, second is max, third is the progress proportion
+            float angle1 = Mathf.Lerp(0, 25, (birdRigBody.velocity.y / 10f)) + 5.0f;
+            Debug.Log("1111: " + angle1.ToString());
+            transform.rotation = Quaternion.Euler(0, 0, angle1);
+        }
+        else
+        {
+            float angle = Mathf.Lerp(0, -90, (-birdRigBody.velocity.y / 30f)) - 5.0f;
+            Debug.Log("222: " + angle.ToString());
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)

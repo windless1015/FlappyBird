ec8a00c700b7ae47478036591b59acfda17f003a
windless1015 <shangtao1989@qq.com>
2022-07-29 20:33:55 -0700

    modify the logic of adding score modify the prefab properties

--- Assets/Scripts/FlyBird.cs
+++ Assets/Scripts/FlyBird.cs
@@ -1,67 +1,182 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlyBird : MonoBehaviour
 {
-    public GameManager gameManager;
-    public float verticalVelocity = 2.4f;
-    //public Vector2 jumpForce = new Vector2(0, 300);
-    private Rigidbody2D birdRigBody;
-    // Start is called before the first frame update
+    public AudioClip FlyAudioClip, DeathAudioClip, ScoredAudioClip;
+    public Sprite GetReadySprite;
+    public float RotateUpSpeed = 1, RotateDownSpeed = 1;
+    public GameObject IntroGUI, DeathGUI;
+    public Collider2D restartButtonGameCollider;
+    public float VelocityPerJump = 3;
+    public float XSpeed = 1;
+    Vector3 birdRotation = Vector3.zero;
 
-    public GameObject bulletPerfab;
+    // Use this for initialization
     void Start()
     {
-        birdRigBody = GetComponent<Rigidbody2D>();
 
-        //birdRigBody.velocity = Vector2.zero;
-        //birdRigBody.AddForce(jumpForce);
+    }
+
+    FlappyYAxisTravelState flappyYAxisTravelState;
+
+    enum FlappyYAxisTravelState
+    {
+        GoingUp, GoingDown
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        //handle back key in Windows Phone
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        if (GameStateManager.GameState == GameState.Introduction)
         {
-            birdRigBody.velocity = Vector2.up * verticalVelocity;
-
-            //birdRigBody.velocity = Vector2.zero;
-            //birdRigBody.AddForce(jumpForce);
-
-        }
-        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown("space")) {
-
-          var go =  GameObject.Instantiate(bulletPerfab, transform.position + new Vector3(0.3f,0,0), Quaternion.identity, null);
-            
-        }
-
-
-        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-        if (screenPosition.y < -1 * Screen.height)
-        {
-            gameManager.GameOver();
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+                GameStateManager.GameState = GameState.Playing;
+                IntroGUI.SetActive(false);
+                ScoreManager.Score = 0;
+            }
         }
 
-        if (birdRigBody.velocity.y > 0)
+        else if (GameStateManager.GameState == GameState.Playing)
         {
-            // first is min, second is max, third is the progress proportion
-            float angle1 = Mathf.Lerp(0, 25, (birdRigBody.velocity.y / 10f)) + 5.0f;
-            //Debug.Log("1111: " + angle1.ToString());
-            transform.rotation = Quaternion.Euler(0, 0, angle1);
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+            }
+
         }
-        else
+
+        else if (GameStateManager.GameState == GameState.Dead)
         {
-            float angle = Mathf.Lerp(0, -90, (-birdRigBody.velocity.y / 30f)) - 5.0f;
-            //Debug.Log("222: " + angle.ToString());
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            Vector2 contactPoint = Vector2.zero;
+
+            if (Input.touchCount > 0)
+                contactPoint = Input.touches[0].position;
+            if (Input.GetMouseButtonDown(0))
+                contactPoint = Input.mousePosition;
+
+            //check if user wants to restart the game
+            if (restartButtonGameCollider == Physics2D.OverlapPoint
+                (Camera.main.ScreenToWorldPoint(contactPoint)))
+            {
+                GameStateManager.GameState = GameState.Introduction;
+                //Application.LoadLevel(Application.loadedLevelName);
+                SceneManager.LoadScene(0);
+            }
         }
 
     }
 
+
+    void FixedUpdate()
+    {
+        //just jump up and down on intro screen
+        if (GameStateManager.GameState == GameState.Introduction)
+        {
+            if (GetComponent<Rigidbody2D>().velocity.y < -1) //when the speed drops, give a boost
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, GetComponent<Rigidbody2D>().mass * 5500 * Time.deltaTime)); //lots of play and stop 
+                                                        //and play and stop etc to find this value, feel free to modify
+        }
+        else if (GameStateManager.GameState == GameState.Playing || GameStateManager.GameState == GameState.Dead)
+        {
+            BirdRotates();
+        }
+    }
+
+    bool WasTouchedOrClicked()
+    {
+        if (Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0) || 
+            (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
+            return true;
+        else
+            return false;
+    }
+
+    void MoveBirdOnXAxis()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+
+    void BoostOnYAxis()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, VelocityPerJump);
+        GetComponent<AudioSource>().PlayOneShot(FlyAudioClip);
+    }
+
+
+
+    private void BirdRotates()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.y > 0) flappyYAxisTravelState = FlappyYAxisTravelState.GoingUp;
+        else flappyYAxisTravelState = FlappyYAxisTravelState.GoingDown;
+
+        float degreesToAdd = 0;
+
+        switch (flappyYAxisTravelState)
+        {
+            case FlappyYAxisTravelState.GoingUp:
+                degreesToAdd = 6 * RotateUpSpeed;
+                break;
+            case FlappyYAxisTravelState.GoingDown:
+                degreesToAdd = -3 * RotateDownSpeed;
+                break;
+            default:
+                break;
+        }
+        //solution with negative eulerAngles found here: http://answers.unity3d.com/questions/445191/negative-eular-angles.html
+
+        //clamp the values so that -90<rotation<45 *always*
+        birdRotation = new Vector3(0, 0, Mathf.Clamp(birdRotation.z + degreesToAdd, -90, 45));
+        transform.eulerAngles = birdRotation;
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (GameStateManager.GameState == GameState.Playing)
+        {
+            //the blank
+            if (col.gameObject.tag == "PipeBlankTrigger")
+            {
+                GetComponent<AudioSource>().PlayOneShot(ScoredAudioClip);
+                ScoreManager.Score++;
+            }
+            else if (col.gameObject.tag == "Pipe")
+            {
+                FlappyDies();
+            }
+        }
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        gameManager.GameOver();
+        if (GameStateManager.GameState == GameState.Playing)
+        {
+            if (collision.gameObject.tag == "Floor")
+            {
+                FlappyDies();
+            }
+        }
+    }
+
+
+    void FlappyDies()
+    {
+        GameStateManager.GameState = GameState.Dead;
+        DeathGUI.SetActive(true);
+        //play the death audio
+        GetComponent<AudioSource>().PlayOneShot(DeathAudioClip);
     }
 }

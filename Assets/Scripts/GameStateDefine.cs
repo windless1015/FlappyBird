392e001db6bde52b6934c8f693aa7e82ab2ccf76
windless1015 <shangtao1989@qq.com>
2022-07-29 18:11:27 -0700

    add game state to be more clear to the game logic

---
+++ Assets/Scripts/GameStateDefine.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public enum GameState
+{
+    Introduction,
+    Playing,
+    Dead
+}
+
+public static class GameStateManager
+{
+    public static GameState GameState { get; set; }
+
+    static GameStateManager ()
+    {
+        GameState = GameState.Introduction;
+    }
+
+
+
+}
